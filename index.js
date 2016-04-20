92028c3825c5b9f4bb0f6283d53b57bb6d4e9663
Yaniv Tal <yanivtalmusic@gmail.com>
2016-04-19 18:36:33 -0700

    remove react native code

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 var parseCss = require('gulp-react-native-stylesheet-css/lib/parseCss');
 
 module.exports = function(source) {
-
-	var style = parseCss(source.replace(/\r?\n|\r/g, ""));
-
-	var prefix = "", suffix = "";
-	prefix = "var React = require('react-native');\nmodule.exports = React.StyleSheet.create(";
-	suffix = ");";
-	var content = prefix + style + suffix
-    return content;
+	return parseCss(source.replace(/\r?\n|\r/g, ""));
 };
